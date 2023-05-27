cbebe7ec7ad23a933be734057fd79884bdcb9797
Raunak Kumar Sah <itisraunak@gmail.com>
2023-05-27 09:59:49 +0530

    Created Admin and sidebar:
    Signed-off-by: Raunak Kumar Sah <itisraunak@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,10 @@ import CoursePage from './components/CoursePage/CoursePage';
 import Profile from './components/Profile/Profile';
 import ChangePassword from './components/Profile/ChangePassword';
 import UpdateProfile from './components/Profile/UpdateProfile';
+import Dashboard from './components/Admin/Dashboard/Dashboard';
+import CreateCourse from './components/Admin/CreateCourse/CreateCourse';
+import AdminCourses from './components/Admin/AdminCourses/AdminCourses';
+import Users from './components/Admin/Users/Users';
 
 function App() {
   window.addEventListener('contextmenu', e => {
@@ -43,9 +47,16 @@ function App() {
         <Route path="/forgetpassword" element={<ForgetPassword />} />
         <Route path="resetpassword/:token" element={<ResetPassword />} />
         <Route path="/subscribe" element={<Subscribe />} />
-        <Route path="*" element={<NotFound />} />
         <Route path="/paymentsuccess" element={<PaymentSuccess />} />
         <Route path="/paymentfail" element={<PaymentFail />} />
+        <Route path="*" element={<NotFound />} />
+
+        {/* Admin Routes */}
+
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/createcourse" element={<CreateCourse />} />
+        <Route path="/admin/courses" element={<AdminCourses />} />
+        <Route path="/admin/users" element={<Users />} />
       </Routes>
       <Footer />
     </Router>
---
+++ frontend/src/components/Admin/AdminCourses/AdminCourses.jsx
@@ -0,0 +1,21 @@
+import { Box, Grid } from '@chakra-ui/react';
+import React from 'react';
+import Sidebar from '../Sidebar';
+import cursor from '../../../assets/images/cursor.png';
+
+const AdminCourses = () => {
+  return (
+    <Grid
+      css={{
+        cursor: `url(${cursor}), default`,
+      }}
+      minH={'100vh'}
+      templateColumns={['1fr', '5fr 1fr']}
+    >
+      <Box></Box>
+      <Sidebar />
+    </Grid>
+  );
+};
+
+export default AdminCourses;
