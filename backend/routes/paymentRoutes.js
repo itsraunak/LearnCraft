fed7f03324bb79061737249135978c882a88d77b
Raunak Kumar Sah <itisraunak@gmail.com>
2023-06-06 11:25:01 +0530

    Created cancel subscription & Payment Verification
    Signed-off-by: Raunak Kumar Sah <itisraunak@gmail.com>

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,11 +1,24 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { buySubscription } from "../controllers/paymentController.js";
+import {
+  buySubscription,
+  cancelSubscription,
+  getRazorPayKey,
+  paymentVerification,
+} from "../controllers/paymentController.js";
 
 const router = express.Router();
 
 // Buy Subscription
-
 router.route("/subscribe").get(isAuthenticated, buySubscription);
+
+// Verify Payment and save Reference in database
+router.route("/paymentverification").post(isAuthenticated, paymentVerification);
+
+// Get Razorpay Key
+router.route("/razorpaykey").get(getRazorPayKey);
+
+// Cancel Subscription
+router.route("/subscribe/cancel").delete(isAuthenticated, cancelSubscription);
 
 export default router;
